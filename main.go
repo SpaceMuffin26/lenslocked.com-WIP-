bc4e7a725c3263b8a6ffed0847b99c708af94cf6
s.diasamidze <saba2006.diasamidze@gmail.com>
2024-12-22 19:08:55 +0400

    3.ex1 added a faq page

--- main.go
+++ main.go
@@ -17,9 +17,15 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:saba2006.diasamidze@gmail.com\">"+"saba2006.diasamidze@gmail.com</a>.")
 }
 
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<p>how are you so awesome?: idk im just like that</p>")
+}
+
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", faq)
 	http.ListenAndServe(":8080", r)
 }
