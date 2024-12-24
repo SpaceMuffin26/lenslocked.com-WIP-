6c52d8cf869fa73e3d24875df897a5f5e0eee953
s.diasamidze <saba2006.diasamidze@gmail.com>
2024-12-24 20:18:19 +0400

    6.1 first view made C:

--- exp/main.go
+++ exp/main.go
@@ -2,19 +2,20 @@ package main
 
 import (
 	"html/template"
-	"os"
+	"net/http"
 )
 
+var homeTemplate *template.Template
+
 func main() {
-	t, err := template.ParseFiles("hello.gohtml")
-	if err != nil {
-		panic(err)
-	}
-	data := struct {
-		Name string
-	}{"John Smith"}
-	err = t.Execute(os.Stdout, data)
-	if err != nil {
+	homeTemplate, _ = template.ParseFiles("views/home.gohtml")
+	http.HandleFunc("/", home)
+	http.ListenAndServe(":3000", nil)
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	if err := homeTemplate.Execute(w, nil); err != nil {
 		panic(err)
 	}
 }
