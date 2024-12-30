0eb6c2559e9134994aac30550702ed52a4cd1ad0
s.diasamidze <saba2006.diasamidze@gmail.com>
2024-12-30 13:29:04 +0400

    6.1.1 Added a Bootstrap signup form

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 var homeView *views.View
 var contactView *views.View
 var faqView *views.View
+var signupView *views.View
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
@@ -26,6 +27,11 @@ func faq(w http.ResponseWriter, r *http.Request) {
 	must(faqView.Render(w, nil))
 }
 
+func signup(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	must(signupView.Render(w, nil))
+}
+
 // helper function that panics on error
 func must(err error) {
 	if err != nil {
@@ -37,10 +43,12 @@ func main() {
 	homeView = views.NewView("bootstrap", "views/home.gohtml")
 	contactView = views.NewView("bootstrap", "views/contact.gohtml")
 	faqView = views.NewView("bootstrap", "views/faq.gohtml")
+	signupView = views.NewView("bootstrap", "views/signup.gohtml")
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
 	r.HandleFunc("/faq", faq)
+	r.HandleFunc("/signup", signup)
 	http.ListenAndServe(":8080", r)
 }
