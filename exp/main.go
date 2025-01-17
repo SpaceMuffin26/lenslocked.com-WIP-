0fab418a5e09d66db156a4e804e8ce23dde3651a
s.diasamidze <saba2006.diasamidze@gmail.com>
2025-01-17 14:10:05 +0400

    did a bit of work for my first database, miserible failure

---
+++ exp/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "1982"
+	dbname   = "saba"
+)
+
+func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Successfully connected!")
+	db.Close()
+}
--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.23.4
 require (
 	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/gorilla/schema v1.4.1 // indirect
+	github.com/lib/pq v1.10.9 // indirect
 )
